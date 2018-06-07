27b73b6ffbeb87a9997c3c42f9591b6313875629
peachykeen5 <lexi.keen@gmail.com>
2018-06-07 23:20:17 +0000

    practicing with maps and callbacks

---
+++ callback-map.js
@@ -0,0 +1,34 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+function map(array, callback) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+};
+
+console.log(map(words, function(word) {
+  return word.length;
+}));
+
+console.log(map(words, function(word) {
+  return word.toUpperCase();
+}));
+
+console.log(map(words, function(word) {
+  return word.split('').reverse().join('');
+}));
