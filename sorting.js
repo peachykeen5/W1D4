fe5d3d1844cbc3716df749d482f2b5d93b2cb14a
Lexi Keen <36801476+peachykeen5@users.noreply.github.com>
2018-06-07 15:03:08 -0700

    learning the sort function

---
+++ sorting.js
@@ -0,0 +1,26 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function compareNames(a, b, c) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  {
+  if (a.age < b.age) {
+      return 1;
+      };
+    return 0;
+  };
+});
+
+
+console.log(students);
